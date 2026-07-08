9d5c28b03ed9eb7e5b27cc142d26830b5669bba0
bootloader24 <bootloader@student.su>
2026-07-08 12:45:29 +0500

    6.5 Практикуемся в использовании API клиентов. Практикуемся в использовании API-клиентов.

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -6,6 +6,17 @@ from clients.api_client import APIClient
 from clients.public_http_builder import get_public_http_client
 
 
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
 class CreateUserRequestDict(TypedDict):
     """
     Описание структуры запроса на создание пользователя.
@@ -15,6 +26,13 @@ class CreateUserRequestDict(TypedDict):
     lastName: str
     firstName: str
     middleName: str
+
+
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
 
 
 class PublicUsersClient(APIClient):
@@ -30,6 +48,10 @@ class PublicUsersClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/users", json=request)
+
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
 
 
 def get_public_users_client() -> PublicUsersClient:
