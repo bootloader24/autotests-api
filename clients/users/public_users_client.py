f8a86d93c0ef8ae4c7b27cb6d068590252dc12af
bootloader24 <bootloader@student.su>
2026-07-08 11:47:20 +0500

    6.5 Практикуемся в использовании API клиентов. Реализуем public builder для HTTP-клиента.

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -3,6 +3,7 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.public_http_builder import get_public_http_client
 
 
 class CreateUserRequestDict(TypedDict):
@@ -14,6 +15,7 @@ class CreateUserRequestDict(TypedDict):
     lastName: str
     firstName: str
     middleName: str
+
 
 class PublicUsersClient(APIClient):
     """
@@ -28,3 +30,12 @@ class PublicUsersClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/users", json=request)
+
+
+def get_public_users_client() -> PublicUsersClient:
+    """
+    Функция создаёт экземпляр PublicUsersClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию PublicUsersClient.
+    """
+    return PublicUsersClient(client=get_public_http_client())
