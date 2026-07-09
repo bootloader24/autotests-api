f202351967bf178b22617e86cfdec8d8e9fd833d
bootloader24 <bootloader@student.su>
2026-07-09 16:25:12 +0500

    7.1 Знакомство с Pydantic. Знакомство с pydantic - alias.

--- pydantic_example.py
+++ pydantic_example.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Address(BaseModel):
@@ -11,6 +11,7 @@ class User(BaseModel):
     name: str
     email: str
     address: Address  # Вложенная модель
+    phone_number: str = Field(alias='phoneNumber') # Алиас для ключа в формате camelCase
     is_active: bool = True  # Значение по умолчанию
 
 
@@ -21,10 +22,12 @@ user = User(
     address={
         "city": "New York",
         "zip_code": "10001"
-    }
+    },
+    phoneNumber="111-222-333" # передаётся значение по alias
 )
 print(user)  # вывод всего объекта user
 print(user.name)  # Alice
 print(user.id)  # 123 (автоматически преобразован в int)
 print(user.address.city)  # New York
+print(user.phone_number)  # 111-222-333
 print(user.model_dump_json())  # вывод в JSON
