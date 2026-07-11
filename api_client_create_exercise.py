388c50cb4b920623ecfd517ea8d0f5679154fca3
bootloader24 <bootloader@student.su>
2026-07-12 02:03:28 +0500

    7.2 Применяем Pydantic в API клиентах. Применяем Pydantic в PublicUsersClient, PrivateUsersClient.

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -2,25 +2,26 @@ from clients.courses.courses_client import get_courses_client, CreateCourseReque
 from clients.exercises.exercises_client import get_exercises_client, CreateExerciseRequestDict
 from clients.files.files_client import get_files_client, CreateFileRequestDict
 from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema
 from tools.fakers import get_random_email
 
 public_users_client = get_public_users_client()
 
 # Создаём пользователя
-create_user_request = CreateUserRequestDict(
+create_user_request = CreateUserRequestSchema(
     email=get_random_email(),
     password="password",
-    lastName="lastName",
-    firstName="firstName",
-    middleName="middleName"
+    last_name="lastName",
+    first_name="firstName",
+    middle_name="middleName"
 )
 create_user_response = public_users_client.create_user(create_user_request)
 
 # Инициализируем клиенты
 authentication_user = AuthenticationUserSchema(
-    email=create_user_request['email'],
-    password=create_user_request['password']
+    email=create_user_request.email,
+    password=create_user_request.password
 )
 files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
@@ -43,7 +44,7 @@ create_course_request = CreateCourseRequestDict(
     description="Basic java course",
     estimatedTime="3 weeks",
     previewFileId=create_file_response['file']['id'],
-    createdByUserId=create_user_response['user']['id']
+    createdByUserId=create_user_response.user.id
 )
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
