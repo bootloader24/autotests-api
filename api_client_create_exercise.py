84c0ed754d42ff50396079c5861d7eccf7fd1f0f
bootloader24 <bootloader@student.su>
2026-07-15 12:32:31 +0500

    7.4 Знакомство с Faker. Практика работы с генерацией случайных данных.

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -7,18 +7,11 @@ from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import fake
 
 public_users_client = get_public_users_client()
 
 # Создаём пользователя
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="password",
-    last_name="lastName",
-    first_name="firstName",
-    middle_name="middleName"
-)
+create_user_request = CreateUserRequestSchema()
 create_user_response = public_users_client.create_user(create_user_request)
 
 # Инициализируем клиенты
@@ -31,21 +24,12 @@ courses_client = get_courses_client(authentication_user)
 exercises_client = get_exercises_client(authentication_user)
 
 # Загружаем файл
-create_file_request = CreateFileRequestSchema(
-    filename="image.png",
-    directory="courses",
-    upload_file="./testdata/files/image.png"
-)
+create_file_request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
 # Создаём курс
 create_course_request = CreateCourseRequestSchema(
-    title="Java",
-    max_score=100,
-    min_score=10,
-    description="Basic java course",
-    estimated_time="3 weeks",
     preview_file_id=create_file_response.file.id,
     created_by_user_id=create_user_response.user.id
 )
@@ -53,14 +37,6 @@ create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
 
 # Создаём задание
-create_exercise_request = CreateExerciseRequestSchema(
-    title="Variables and Types",
-    course_id=create_course_response.course.id,
-    max_score=10,
-    min_score=1,
-    order_index=1,
-    description="This exercise will go over a few basic types of variables",
-    estimated_time="2 hours"
-)
+create_exercise_request = CreateExerciseRequestSchema(course_id=create_course_response.course.id)
 create_exercise_response = exercises_client.create_exercise(create_exercise_request)
 print('Create exercise data:', create_exercise_response)
