953bd3b08453d60d5f32d3e3279da7dc71b1ecc3
bootloader24 <bootloader@student.su>
2026-07-15 11:55:05 +0500

    7.4 Знакомство с Faker. Применение Fake в Pydantic-моделях.

--- clients/authentication/authentication_schema.py
+++ clients/authentication/authentication_schema.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field, ConfigDict
 
+from tools.fakers import fake
+
 
 class TokenSchema(BaseModel):
     """
@@ -16,8 +18,8 @@ class LoginRequestSchema(BaseModel):
     """
     Описание структуры запроса на аутентификацию.
     """
-    email: str
-    password: str
+    email: str = Field(default_factory=fake.email)
+    password: str = Field(default_factory=fake.password)
 
 
 class LoginResponseSchema(BaseModel):
@@ -33,4 +35,4 @@ class RefreshRequestSchema(BaseModel):
     """
     model_config = ConfigDict(validate_by_name=True)
 
-    refresh_token: str = Field(alias="refreshToken")
+    refresh_token: str = Field(alias="refreshToken", default_factory=fake.sentence)
