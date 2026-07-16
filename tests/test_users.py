b3f4f3543ad95ea27cdfba5ab8ecf65c605e46f9
bootloader24 <bootloader@student.su>
2026-07-16 16:21:15 +0500

    8.2 Написание API автотестов используя Pytest. Улучшаем первый API автотест.

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,6 +2,9 @@ from http import HTTPStatus
 
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
+from tools.assertions.users import assert_create_user_response
 
 
 def test_create_user():
@@ -17,10 +20,10 @@ def test_create_user():
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
     # Проверяем статус-код ответа, используя enum из HTTPStatus
-    assert response.status_code == HTTPStatus.OK, 'Некорректный статус-код ответа'
+    assert_status_code(response.status_code, HTTPStatus.OK)
 
     # Проверяем, что данные ответа совпадают с данными запроса
-    assert response_data.user.email == request.email, 'Некорректный email пользователя'
-    assert response_data.user.last_name == request.last_name, 'Некорректный last_name пользователя'
-    assert response_data.user.first_name == request.first_name, 'Некорректный first_name пользователя'
-    assert response_data.user.middle_name == request.middle_name, 'Некорректный middle_name пользователя'
+    assert_create_user_response(request, response_data)
+
+    # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
+    validate_json_schema(response.json(), response_data.model_json_schema())
