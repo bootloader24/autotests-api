b3ac723b662dad15bbf38ffd59f631c6872c81ad
bootloader24 <bootloader@student.su>
2026-07-16 12:48:01 +0500

    8.1 Введение в Pytest. Тестовые функции и классы.

---
+++ tests/test_pytest.py
@@ -0,0 +1,10 @@
+# Тестовые функции
+def test_user_login():
+    assert 1 == 1
+
+
+# Тестовые классы
+class TestUserAuthentication:
+    # Конструктора (__init__()) в тестовых классах быть не должно
+    def test_login(self):
+        assert 1 == 1
