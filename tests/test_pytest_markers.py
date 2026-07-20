a4cf8ec082eec70191aea78856a8bf37c4d6f0c7
bootloader24 <bootloader@student.su>
2026-07-20 12:38:37 +0500

    8.3 Маркировки Pytest. Сложные pytest маркировки.

--- tests/test_pytest_markers.py
+++ tests/test_pytest_markers.py
@@ -35,3 +35,54 @@ class TestSuite:
 
     def test_case2(self):
         ...
+
+
+# Маркировка класса и отдельных тестов внутри класса
+# Запуск тестов, которые имеют маркировку regression и не имеют маркировки slow:
+# pytest -m "regression and not slow"
+@pytest.mark.regression  # действует на все тесты класса
+class TestUserAuthentication:
+
+    @pytest.mark.smoke
+    def test_login(self):
+        pass
+
+    @pytest.mark.slow
+    def test_password_reset(self):  # для запуска только этого теста: pytest -m "regression and slow"
+        pass
+
+    def test_logout(self):
+        pass
+
+
+# Несколько маркировок на одном тесте
+# Запуск тестов с маркировкой critical: pytest -m "critical"
+# Запуск всех smoke или critical тестов: pytest -m "smoke or critical"
+@pytest.mark.smoke
+@pytest.mark.regression
+@pytest.mark.critical
+def test_critical_login():
+    pass
+
+
+# Все тесты класса TestUserInterface помечены как api-тесты
+# Тест test_login также помечен как smoke и critical
+# Тест test_forgot_password относится к регрессионным
+# Тест test_signup помечен как smoke
+# Запуск только smoke тестов: pytest -m "smoke"
+# Запуск тестов, которые помечены как api и одновременно относятся к regression: pytest -m "api and regression"
+@pytest.mark.api
+class TestUserInterface:
+
+    @pytest.mark.smoke
+    @pytest.mark.critical
+    def test_login(self):
+        pass
+
+    @pytest.mark.regression
+    def test_forgot_password(self):
+        pass
+
+    @pytest.mark.smoke
+    def test_signup(self):
+        pass
