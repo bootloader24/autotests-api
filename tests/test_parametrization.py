d5face0ad4a780d4c64b8b4767415a40323a1409
bootloader24 <bootloader@student.su>
2026-07-21 16:28:08 +0500

    8.7 Параметризация Pytest. Продолжаем практиковаться в работе с параметризацией.

--- tests/test_parametrization.py
+++ tests/test_parametrization.py
@@ -1,14 +1,15 @@
 import pytest
+from _pytest.fixtures import SubRequest
 
 
-# Базовый случай
+# --- Базовый случай ---
 @pytest.mark.parametrize("number", [1, 2, 3, -1])  # Параметризируем тест
 # Название "number" в декораторе "parametrize" и в аргументах автотеста должны совпадать
 def test_numbers(number: int):
     assert number > 0
 
 
-# Несколько параметров
+# --- Несколько параметров ---
 @pytest.mark.parametrize("number, expected", [(1, 1), (2, 4), (3, 9)])
 # В данном случае в качестве данных используется список с кортежами
 def test_several_numbers(number: int, expected: int):
@@ -16,7 +17,7 @@ def test_several_numbers(number: int, expected: int):
     assert number ** 2 == expected
 
 
-# Перемножение параметров
+# --- Перемножение параметров ---
 @pytest.mark.parametrize("os", ["macos", "windows", "linux", "debian"])  # Параметризируем по операционной системе
 @pytest.mark.parametrize("host", [
     "https://dev.company.com",
@@ -25,3 +26,36 @@ def test_several_numbers(number: int, expected: int):
 ])  # Параметризируем по хосту
 def test_multiplication_of_numbers(os: str, host: str):
     assert len(os + host) > 0  # Проверка указана для примера
+
+
+# --- Параметризация фикстур ---
+@pytest.fixture(params=[
+    "https://dev.company.com",
+    "https://stable.company.com",
+    "https://prod.company.com"
+])
+# Фикстура будет возвращать три разных хоста
+# Соответственно все автотесты использующие данную фикстуру будут запускаться три раза
+def host(request: SubRequest) -> str:
+    # Внутри атрибута param находится одно из значений "https://dev.company.com",
+    # "https://stable.company.com", "https://prod.company.com"
+    return request.param
+
+
+# В самом автотесте уже не нужно добавлять параметризацию, он будет автоматически параметризован из фикстуры
+def test_host(host: str):
+    # Используем фикстуру в автотесте, она вернет нам хост в виде строки
+    print(f"Running test on host: {host}")
+
+
+# --- Параметризация классов ---
+# Для тестовых классов параметризация указывается для самого класса
+@pytest.mark.parametrize("user", ["Alice", "Zara"])
+class TestOperations:
+    # Параметр "user" передается в качестве аргумента в каждый тестовый метод класса
+    def test_user_with_operations(self, user: str):
+        print(f"User with operations: {user}")
+
+    # Аналогично тут передается "user"
+    def test_user_without_operations(self, user: str):
+        print(f"User without operations: {user}")
