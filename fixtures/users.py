5aae1f25e7b9d605d566946fa8b9a0a1a9678080
bootloader24 <bootloader@student.su>
2026-07-21 16:10:10 +0500

    8.6 Плагины Pytest. Реорганизация проекта с использованием плагинов.

--- fixtures/users.py
+++ fixtures/users.py
@@ -1,9 +1,8 @@
 import pytest
 from pydantic import BaseModel, EmailStr
 
-from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
 from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
+from clients.users.private_users_client import get_private_users_client, PrivateUsersClient
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 
@@ -27,11 +26,6 @@ class UserFixture(BaseModel):
             email=self.email,
             password=self.password
         )
-
-
-@pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    return get_authentication_client()
 
 
 @pytest.fixture
