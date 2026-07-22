6b1d95d686f1b5280fedc23cdc6b13faad58abde
bootloader24 <bootloader@student.su>
2026-07-22 13:26:09 +0500

    9.1 Рефакторинг и подготовка к написанию API автотестов. Использование тестовых классов.

--- tests/authentication/test_authentication.py
+++ tests/authentication/test_authentication.py
@@ -12,22 +12,24 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login(
-        function_user: UserFixture,  # Используем фикстуру для создания пользователя
-        authentication_client: AuthenticationClient
-):
-    # Запрос на логин (login_request -> request)
-    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
-    # Выполняем логин (login_response -> response)
-    response = authentication_client.login_api(request)
-    # Валидация ответа (login_response_data -> response_data)
-    response_data = LoginResponseSchema.model_validate_json(response.text)
+class TestAuthentication:
+    def test_login(
+            self,
+            function_user: UserFixture,  # Используем фикстуру для создания пользователя
+            authentication_client: AuthenticationClient
+    ):
+        # Запрос на логин (login_request -> request)
+        request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+        # Выполняем логин (login_response -> response)
+        response = authentication_client.login_api(request)
+        # Валидация ответа (login_response_data -> response_data)
+        response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    # Проверяем статус-код ответа
-    assert_status_code(response.status_code, HTTPStatus.OK)
+        # Проверяем статус-код ответа
+        assert_status_code(response.status_code, HTTPStatus.OK)
 
-    # Проверяем корректность тела ответа
-    assert_login_response(response_data)
+        # Проверяем корректность тела ответа
+        assert_login_response(response_data)
 
-    # Проверяем, что ответ соответствует ожидаемой JSON-схеме
-    validate_json_schema(response.json(), response_data.model_json_schema())
+        # Проверяем, что ответ соответствует ожидаемой JSON-схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -14,40 +14,42 @@ from tools.fakers import fake
 
 @pytest.mark.users
 @pytest.mark.regression
-@pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])  # Параметризируем по domain в email
-def test_create_user(domain: str, public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
-    # Формируем тело запроса на создание пользователя с параметризованным доменом в поле email
-    request = CreateUserRequestSchema(email=fake.email(domain=domain))
-    # Отправляем запрос на создание пользователя
-    response = public_users_client.create_user_api(request)
-    # Инициализируем модель ответа на основе полученного JSON в ответе
-    # Также благодаря встроенной валидации в Pydantic дополнительно убеждаемся, что ответ корректный
-    response_data = CreateUserResponseSchema.model_validate_json(response.text)
+class TestUsers:
+    @pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])  # Параметризируем по domain в email
+    def test_create_user(self, domain: str, public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
+        # Формируем тело запроса на создание пользователя с параметризованным доменом в поле email
+        request = CreateUserRequestSchema(email=fake.email(domain=domain))
+        # Отправляем запрос на создание пользователя
+        response = public_users_client.create_user_api(request)
+        # Инициализируем модель ответа на основе полученного JSON в ответе
+        # Также благодаря встроенной валидации в Pydantic дополнительно убеждаемся, что ответ корректный
+        response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
-    # Проверяем статус-код ответа, используя enum из HTTPStatus
-    assert_status_code(response.status_code, HTTPStatus.OK)
+        # Проверяем статус-код ответа, используя enum из HTTPStatus
+        assert_status_code(response.status_code, HTTPStatus.OK)
 
-    # Проверяем, что данные ответа совпадают с данными запроса
-    assert_create_user_response(request, response_data)
+        # Проверяем, что данные ответа совпадают с данными запроса
+        assert_create_user_response(request, response_data)
 
-    # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
-    validate_json_schema(response.json(), response_data.model_json_schema())
+        # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
 
+    def test_get_user_me(
+            self,
+            private_users_client: PrivateUsersClient,
+            function_user: UserFixture
+    ):
+        # Отправляем GET-запрос /api/v1/users/me на получение данных о "своём" пользователе
+        response = private_users_client.get_user_me_api()
 
-@pytest.mark.users
-@pytest.mark.regression
-def test_get_user_me(private_users_client: PrivateUsersClient, function_user: UserFixture):
-    # Отправляем GET-запрос /api/v1/users/me на получение данных о "своём" пользователе
-    response = private_users_client.get_user_me_api()
+        # Инициализируем модель ответа на основе полученного JSON в ответе c проверкой его корректности
+        response_data = GetUserResponseSchema.model_validate_json(response.text)
 
-    # Инициализируем модель ответа на основе полученного JSON в ответе c проверкой его корректности
-    response_data = GetUserResponseSchema.model_validate_json(response.text)
+        # Проверяем статус-код ответа
+        assert_status_code(response.status_code, HTTPStatus.OK)
 
-    # Проверяем статус-код ответа
-    assert_status_code(response.status_code, HTTPStatus.OK)
+        # Сравниваем тело ответа с данными ранее созданного пользователя
+        assert_get_user_response(response_data, function_user.response)
 
-    # Сравниваем тело ответа с данными ранее созданного пользователя
-    assert_get_user_response(response_data, function_user.response)
-
-    # Валидируем тело ответа на соответствие ожидаемой JSON-схеме
-    validate_json_schema(response.json(), response_data.model_json_schema())
+        # Валидируем тело ответа на соответствие ожидаемой JSON-схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
