b557354a7f215f2843a6dab0c4243cc3776a13e1
bootloader24 <bootloader@student.su>
2026-07-23 13:29:10 +0500

    9.3 Практикуемся в написании API автотестов. Часть 2. Пишем API автотест для получения списка курсов.

--- clients/courses/courses_schema.py
+++ clients/courses/courses_schema.py
@@ -30,6 +30,13 @@ class GetCoursesQuerySchema(BaseModel):
     user_id: str = Field(alias="userId")
 
 
+class GetCoursesResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на получение списка курсов.
+    """
+    courses: list[CourseSchema]
+
+
 class CreateCourseRequestSchema(BaseModel):
     """
     Описание структуры запроса на создание курса.
