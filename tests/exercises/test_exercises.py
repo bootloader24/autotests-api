92a1166e689b3f039d823e51949479f0629008a2
bootloader24 <bootloader@student.su>
2026-07-25 01:32:43 +0500

    9.4 Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение списка заданий.

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -5,12 +5,13 @@ import pytest
 from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema, GetExercisesQuerySchema, \
+    GetExercisesResponseSchema
 from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response, assert_exercise_not_found_response
+    assert_update_exercise_response, assert_exercise_not_found_response, assert_get_exercises_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -98,3 +99,24 @@ class TestExercises:
 
         # Валидируем JSON-схему ответа
         validate_json_schema(get_response.json(), get_response_data.model_json_schema())
+
+    def test_get_exercises(
+            self,
+            exercises_client: ExercisesClient,
+            function_exercise: ExerciseFixture,
+            function_course: CourseFixture
+    ):
+        # Формируем параметры запроса, передавая course_id из function_course
+        query = GetExercisesQuerySchema(course_id=function_course.response.course.id)
+        # Отправляем GET-запрос на получение списка заданий
+        response = exercises_client.get_exercises_api(query)
+        # Десериализуем JSON-ответ в Pydantic-модель
+        response_data = GetExercisesResponseSchema.model_validate_json(response.text)
+
+        # Проверяем, что код ответа 200 OK
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        # Проверяем, что список заданий соответствует ранее созданным заданиям
+        assert_get_exercises_response(response_data, [function_exercise.response])
+
+        # Проверяем соответствие JSON-ответа схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
