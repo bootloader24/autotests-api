0b9f2b1ed4e8f624a425d720d9dc96951fa7150c
bootloader24 <bootloader@student.su>
2026-07-25 21:19:39 +0500

    10.2 Интеграция Allure в автотесты. Allure аннотации: @allure.epic, @allure.feature, @allure.story.

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -7,6 +7,9 @@ from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from fixtures.users import UserFixture
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
 from tools.allure.tags import AllureTag
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
@@ -17,10 +20,13 @@ from tools.fakers import fake
 @pytest.mark.users
 @pytest.mark.regression
 @allure.tag(AllureTag.USERS, AllureTag.REGRESSION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.USERS)
 class TestUsers:
 
     @pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])  # Параметризируем по domain в email
     @allure.tag(AllureTag.CREATE_ENTITY)
+    @allure.story(AllureStory.CREATE_ENTITY)
     @allure.title("Create user")
     def test_create_user(self, domain: str, public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
         # Формируем тело запроса на создание пользователя с параметризованным доменом в поле email
@@ -41,6 +47,7 @@ class TestUsers:
         validate_json_schema(response.json(), response_data.model_json_schema())
 
     @allure.tag(AllureTag.GET_ENTITY)
+    @allure.story(AllureStory.GET_ENTITY)
     @allure.title("Get user me")
     def test_get_user_me(
             self,
