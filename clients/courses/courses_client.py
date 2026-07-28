0d150efd2bdcf623b8200f18685585e1e6481d59
bootloader24 <bootloader@student.su>
2026-07-28 11:54:03 +0500

    10.3 Добавление Allure шагов в автотесты. Добавление allure.step в API-клиенты.

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -1,3 +1,4 @@
+import allure
 from httpx import Response, QueryParams
 
 from clients.api_client import APIClient
@@ -11,6 +12,7 @@ class CoursesClient(APIClient):
     Клиент для работы с /api/v1/courses
     """
 
+    @allure.step("Get courses")
     def get_courses_api(self, query: GetCoursesQuerySchema) -> Response:
         """
         Метод получения списка курсов.
@@ -23,6 +25,7 @@ class CoursesClient(APIClient):
             params=query.model_dump(by_alias=True)
         )
 
+    @allure.step("Get course by id {course_id}")
     def get_course_api(self, course_id: str) -> Response:
         """
         Метод получения курса.
@@ -32,6 +35,7 @@ class CoursesClient(APIClient):
         """
         return self.get(f"/api/v1/courses/{course_id}")
 
+    @allure.step("Create course")
     def create_course_api(self, request: CreateCourseRequestSchema) -> Response:
         """
         Метод создания курса.
@@ -45,6 +49,7 @@ class CoursesClient(APIClient):
             json=request.model_dump(by_alias=True)
         )
 
+    @allure.step("Update course by id {course_id}")
     def update_course_api(self, course_id: str, request: UpdateCourseRequestSchema) -> Response:
         """
         Метод обновления курса.
@@ -58,6 +63,7 @@ class CoursesClient(APIClient):
             json=request.model_dump(by_alias=True)
         )
 
+    @allure.step("Delete course by id {course_id}")
     def delete_course_api(self, course_id: str) -> Response:
         """
         Метод удаления курса.
