227882b6eca4079bdc82c65bb2c33cb096f7467b
bootloader24 <bootloader@student.su>
2026-07-28 19:33:46 +0500

    11.1 Настройки автотестов с Pydantic. Создаем .env файл с настройками.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, HttpUrl, FilePath
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class HTTPClientConfig(BaseModel):
@@ -16,5 +16,11 @@ class TestDataConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_nested_delimiter=".",
+    )
+
     test_data: TestDataConfig
     http_client: HTTPClientConfig
