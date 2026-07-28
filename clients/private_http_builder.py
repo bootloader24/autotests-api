80decfad2df430431098bfd1e90180630376f7cf
bootloader24 <bootloader@student.su>
2026-07-28 12:39:43 +0500

    10.4 cURL в Allure-отчете: упрощаем анализ API-тестов. Прикрепляем cURL команду к Allure отчету.

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema
+from clients.event_hooks import curl_event_hook
 
 
 # Структура данных пользователя для авторизации
@@ -33,6 +34,6 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:  # Соз
     return Client(
         timeout=100,
         base_url="http://localhost:8000",
-        # Добавляем заголовок авторизации
-        headers={"Authorization": f"Bearer {login_response.token.access_token}"}
+        headers={"Authorization": f"Bearer {login_response.token.access_token}"}, # Добавляем заголовок авторизации
+        event_hooks={"request": [curl_event_hook]}  # Добавляем event hook для запроса
     )
