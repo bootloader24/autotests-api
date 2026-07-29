ec09eff64e7df7aeceecd871cb336a1e2df1a0f0
bootloader24 <bootloader@student.su>
2026-07-29 15:48:07 +0500

    11.3 Добавляем логирование для API автотестов. Создание собственного логгера и использование его в автотестах.

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema
-from clients.event_hooks import curl_event_hook
+from clients.event_hooks import curl_event_hook, log_request_event_hook, log_response_event_hook
 from config import settings
 
 
@@ -36,5 +36,8 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:  # Соз
         timeout=settings.http_client.timeout,  # Используем значение таймаута из настроек
         base_url=settings.http_client.client_url,  # Используем значение адреса сервера из настроек
         headers={"Authorization": f"Bearer {login_response.token.access_token}"}, # Добавляем заголовок авторизации
-        event_hooks={"request": [curl_event_hook]}  # Добавляем event hook для запроса
+        event_hooks={  # Прикрепление cURL запроса к отчёту, логирование запросов и ответов
+            "request": [curl_event_hook, log_request_event_hook],
+            "response": [log_response_event_hook]
+        }
     )
