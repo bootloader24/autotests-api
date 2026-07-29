816e805c42e1d42277c2ed1dc25cc3905f425d03
bootloader24 <bootloader@student.su>
2026-07-29 16:12:28 +0500

    11.3 Добавляем логирование для API автотестов. Добавление логирования в проверки.

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -14,12 +14,12 @@ def assert_create_exercise_response(
         response: CreateExerciseResponseSchema
 ):
     """
-        Проверяет, что ответ на создание задания соответствует данным из запроса.
+    Проверяет, что ответ на создание задания соответствует данным из запроса.
 
-        :param request: Исходный запрос на создание задания.
-        :param response: Ответ API с данными созданного задания.
-        :raises AssertionError: Если хотя бы одно поле не совпадает.
-        """
+    :param request: Исходный запрос на создание задания.
+    :param response: Ответ API с данными созданного задания.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
     assert_equal(response.exercise.title, request.title, "title")
     assert_equal(response.exercise.course_id, request.course_id, "course_id")
     assert_equal(response.exercise.max_score, request.max_score, "max_score")
