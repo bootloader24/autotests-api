702e7480071e76681fae9e672cdae429d00255e6
bootloader24 <bootloader@student.su>
2026-07-30 23:48:14 +0500

    12.4 Запуск автотестов на GitHub Actions. Первый запуск автотестов на CI: GitHub Actions.

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ class TestDataConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
+        extra='allow',  # Разрешаем дополнительные переменные, требуемые для работы серверной части или CI/CD процесса
         env_file=".env",
         env_file_encoding="utf-8",
         env_nested_delimiter=".",
